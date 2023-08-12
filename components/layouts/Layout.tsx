6065d576030c32f29228165228634a75273b92de
Leonardo Rincon <jleobotache@gmail.com>
2023-08-12 17:00:33 -0500

    feat: add meta tags for SEO

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -9,6 +9,8 @@ interface Props {
  tittle?: string;
 }
 
+const origin = typeof window === 'undefined' ? '' : window.location.origin;
+
 export const Layout: FC<Props> = ({ children, tittle }) => {
  return (
   <>
@@ -20,6 +22,13 @@ export const Layout: FC<Props> = ({ children, tittle }) => {
      content={`Information about the Pokemon: ${tittle}`}
     />
     <meta name='keywords' content={`${tittle}, Pokemon, pokedex`} />
+
+    <meta property='og:title' content={`Information about ${tittle}`} />
+    <meta
+     property='og:description'
+     content={`This page contains information about the Pokemon: ${tittle}`}
+    />
+    <meta property='og:image' content={`${origin}/img/bannerPokemon.png`} />
    </Head>
 
    <NavBar />
